3316d4e89c541892fe82e923fb566622e106d977
Kadia-D <kddellor@gmail.com>
2024-03-07 10:02:03 +0000

    new changes

---
+++ index.js
@@ -0,0 +1,28 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.getElementById("send-btn");
+const chatbox = document.querySelector(".chatbox");
+
+let userMessage;
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+    chatLi.innerHTML = chatContent;
+    return chatLi;
+}
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if (!userMessage) return;
+
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    
+    // Clear the input after sending the message
+    chatInput.value = "";
+
+    // Scroll to the bottom of the chatbox
+    chatbox.scrollTop = chatbox.scrollHeight;
+}
+
+sendChatBtn.addEventListener("click", handleChat);
